b4558299bcc74c216fb9700f8b2448067a974ea6
Pedro Perez <pedrojperez1@gmail.com>
2021-01-26 11:15:34 -0500

    add check for mobile devices

--- client/src/components/NewLadderStep0.js
+++ client/src/components/NewLadderStep0.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { Container, Fade } from "reactstrap";
+import { Button, Container, Fade } from "reactstrap";
 
 const NewLadderStep0 = () => {
     const history = useHistory();
@@ -15,14 +15,26 @@ const NewLadderStep0 = () => {
             window.removeEventListener("keydown", handleKeyPress)
         }
     }, [history])
-
+    const nextButtonOrText = () => {
+        if (/Mobi|Android/i.test(navigator.userAgent)) {
+            return (
+                <Button onClick={() => history.push("/newladder/1")}>Continue</Button>
+            )
+        } else {
+            return (
+                <>
+                    Press <kbd>Enter</kbd> to continue
+                </>
+            )
+        }
+    }
     return (
         <Fade>
             <div className="NewLadderStep0 mt-5">
                 <Container className="text-left">
                     <blockquote className="blockquote">Now we will walk you through the process of creating a new fear ladder.</blockquote>
                     <blockquote className="blockquote">Let's get started!</blockquote>
-                    <p class="lead mt-5">Press <kbd>Enter</kbd> to continue</p>
+                    <p class="lead mt-5">{nextButtonOrText()}</p>
                 </Container>
             </div>
         </Fade>
