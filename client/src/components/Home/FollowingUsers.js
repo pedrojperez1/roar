256e238d4271bd6bbb6da0654ed52b92c4a71354
Pedro Perez <pedrojperez1@gmail.com>
2021-03-04 18:51:42 -0500

    fixing bug by removing search bar from FollowingTab and moving to future Following page

--- client/src/components/Home/FollowingUsers.js
+++ client/src/components/Home/FollowingUsers.js
@@ -1,44 +1,31 @@
-import React, { useState } from "react"
-import { Text, Heading, HStack, Spacer, InputRightAddon, InputGroup, Input, Center, Button, Stack } from "@chakra-ui/react"
+import React from "react"
+import { Text, Heading, Center, Button, Stack } from "@chakra-ui/react"
 import UserList from "../UserList/UserList"
-import { Search2Icon } from "@chakra-ui/icons"
 
 const FollowingUsers = ({ users, refetch }) => {
-  const [showUsers, setShowUsers] = useState(users.slice(0, 5))
-  console.log("showUsers", showUsers)
-  const handleSearch = e => {
-    console.log(e.target.value)
-    const filtered = users.filter(u => u.username.includes(e.target.value))
-    setShowUsers(filtered.slice(0, 5))
-  }
+
   return (
     <div className="FollowingUsers">
-      <HStack spacing={10}
+      <Heading 
+        fontSize="24px" 
         mb={5}
         pb="10px"
         style={{ borderBottom: "1px solid rgb(226, 232, 240)", textAlign: "left" }}
-      >
-        <Heading fontSize="24px">Following</Heading>
-        <Spacer />
-        <InputGroup>
-          <Input onChange={handleSearch}/>
-          <InputRightAddon><Search2Icon /></InputRightAddon>
-        </InputGroup>
-      </HStack>
+      >Following</Heading>
       <Stack spacing={5}>
         {users.length === 0 ? (
           <Text>No Followers yet</Text>
         ) : (
           <UserList
-            users={showUsers}
+            users={users.slice(0, 3)}
             type="following"
             refetch={refetch}
           />
         )}
         {
-          showUsers.length > 4 &&
+          users.length > 3 &&
           <Center>
-            <Button size="sm" variant="ghost" colorScheme="purple">See more</Button>
+            <Button size="sm" variant="ghost" colorScheme="purple">See all</Button>
           </Center>
         }
       </Stack>
